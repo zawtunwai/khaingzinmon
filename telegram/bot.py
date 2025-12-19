5ab0d403a835d8671b51b4373115d5aa6446b3b5
zawtunwai <zivpn.delivery@gmail.com>
2025-12-20 00:33:59 +0700

    Update bot.py

--- telegram/bot.py
+++ telegram/bot.py
@@ -22,7 +22,7 @@ logger = logging.getLogger(__name__)
 
 # Configuration
 DATABASE_PATH = os.environ.get("DATABASE_PATH", "/etc/zivpn/zivpn.db")
-BOT_TOKEN = "8299491463:AAEfa06uMm607BYNJC4AUhrvyAF0"
+BOT_TOKEN = "8589710728:AAH92e3A2zoPaH2K2lDXBUlUWp2y7UL0Pvc"
 CONFIG_FILE = "/etc/zivpn/config.json"
 
 # Admin configuration - ONLY YOUR ID CAN SEE ADMIN COMMANDS
